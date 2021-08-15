671e2b4b9a9cc16e0387d2f00bbfa5f90d3f576a
qqz <chance.zhao@gmail.com>
2021-08-15 03:40:27 -0400

    initial commit

---
+++ src/edit.js
@@ -0,0 +1,53 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps, RichText, InnerBlocks } from '@wordpress/block-editor';
+const { Dashicon } = wp.components;
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit(props) {
+	const blockProps = useBlockProps();
+	console.log(blockProps);
+	return (
+		<div { ...blockProps }>
+			<div className="accordion-summary">
+				<span class="dashicons dashicons-arrow-down"></span>
+				<RichText
+					placeholder={__("Accordion title")}
+					onChange={(new_val) => {
+						props.setAttributes({ title: new_val });
+					}}
+					value={props.attributes.title}
+					className="summary-text"
+				/>
+			</div>
+			<div className="accordion-details">
+				<InnerBlocks />
+			</div>
+		</div>
+	);
+}
